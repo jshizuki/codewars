b62e332c678a6bf1cd7451eb65db721134ee2404
Joyce Lau <jshizuki@yahoo.com.hk>
2024-03-20 23:16:42 +0900

    Added solution

--- Ruby/fizz_buzz.rb
+++ Ruby/fizz_buzz.rb
@@ -1,6 +1,5 @@
 def fizzbuzz(n)
-  array = (1..n).to_a
-  array.map do |number|
+  (1..n).to_a.map do |number|
     if number % 3 == 0 && number % 5 == 0 then number = "FizzBuzz"
     elsif number % 3 == 0 then number = "Fizz"
     elsif number % 5 == 0 then number = "Buzz"
