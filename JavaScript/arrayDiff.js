7e4763fbbd9d2e9b052e451dbf189c1f8b4aa6a6
Joyce Lau <jshizuki@yahoo.com.hk>
2024-05-03 22:12:20 +0900

    Add second solution

--- JavaScript/arrayDiff.js
+++ JavaScript/arrayDiff.js
@@ -1,10 +1,16 @@
+// function arrayDiff(arr1, arr2) {
+//   if (arr2[0] === undefined) {
+//     return arr1
+//   } else {
+//     arr1.map((num, index) => arr2.includes(num) ? delete arr1[index] : num)
+//     return arr1.filter(each => each !== undefined)
+//   }
+// }
+
+// console.log(arrayDiff([1,2,3], [1,2]))
+
 function arrayDiff(arr1, arr2) {
-  if (arr2[0] === undefined) {
-    return arr1
-  } else {
-    arr1.map((num, index) => arr2.includes(num) ? delete arr1[index] : num)
-    return arr1.filter(each => each !== undefined)
-  }
+  return arr1.filter(num => !arr2.includes(num))
 }
 
-console.log(arrayDiff([1,2,3], [1,2]))
+console.log(arrayDiff([1,2,2,2,3],[2]))
