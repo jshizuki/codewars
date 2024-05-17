ee6f56619fdb6902c795ba97c0444deb694efcba
Joyce Lau <jshizuki@yahoo.com.hk>
2024-05-17 15:34:22 +0900

    Add solution

---
+++ JavaScript/tribonacciSequence.js
@@ -0,0 +1,13 @@
+function tribonacci(signature,n){
+  if (n < 3) {
+    return signature.slice(0, n)
+  }
+
+  for (let i = 0; i < n - 3; i ++) {
+    const sum = signature.slice(i).reduce((x, y) => x + y)
+    signature.push(sum)
+  }
+  return signature
+}
+
+console.log(tribonacci([1,1,1], 10))
