301a73b728a04d186ea1fabd3bf8f9aabe867513
Joyce Lau <jshizuki@yahoo.com.hk>
2024-05-27 14:15:02 +0900

    Refactor solution

--- JavaScript/changeItUp.js
+++ JavaScript/changeItUp.js
@@ -2,26 +2,49 @@ function changer(str) {
   let arrayOfLetters = Array.from({ length: 26 }, (_, i) =>
     String.fromCharCode(97 + i)
   );
-  const str_split = str.split("");
   arrayOfLetters.push("a");
-  arrayOfLetters.replace();
+  const str_split = str.split("");
 
-  const result = str_split.map((each) => {
-    if (each === each.toLowerCase() && arrayOfLetters.includes(each)) {
-      const result = arrayOfLetters[arrayOfLetters.indexOf(each) + 1];
-      return /[aeiou]/.test(result) ? result.toUpperCase() : result;
-    } else if (
-      each !== each.toLowerCase() &&
-      arrayOfLetters.includes(each.toLowerCase())
-    ) {
-      const result =
-        arrayOfLetters[arrayOfLetters.indexOf(each.toLowerCase()) + 1];
-      return /[aeiou]/.test(result) ? result.toUpperCase() : result;
+  const result = str_split.map((char) => {
+    const indexOfChar = arrayOfLetters.indexOf(char.toLowerCase());
+    //     console.log(indexOfChar)
+
+    if (indexOfChar !== -1) {
+      const nextChar = arrayOfLetters[indexOfChar + 1];
+      return /[aeiou]/.test(nextChar) ? nextChar.toUpperCase() : nextChar;
     } else {
-      return each;
+      return char;
     }
   });
+
   return result.join("");
 }
 
-console.log(changer("Alice"));
+console.log(changer("Catz30"));
+
+// function changer(str) {
+//   let arrayOfLetters = Array.from({ length: 26 }, (_, i) =>
+//     String.fromCharCode(97 + i)
+//   );
+//   const str_split = str.split("");
+//   arrayOfLetters.push("a");
+
+//   const result = str_split.map((each) => {
+//     if (each === each.toLowerCase() && arrayOfLetters.includes(each)) {
+//       const result = arrayOfLetters[arrayOfLetters.indexOf(each) + 1];
+//       return /[aeiou]/.test(result) ? result.toUpperCase() : result;
+//     } else if (
+//       each !== each.toLowerCase() &&
+//       arrayOfLetters.includes(each.toLowerCase())
+//     ) {
+//       const result =
+//         arrayOfLetters[arrayOfLetters.indexOf(each.toLowerCase()) + 1];
+//       return /[aeiou]/.test(result) ? result.toUpperCase() : result;
+//     } else {
+//       return each;
+//     }
+//   });
+//   return result.join("");
+// }
+
+// console.log(changer("Alice"));
