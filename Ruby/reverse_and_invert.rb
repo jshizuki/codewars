15d1622eddc41dc5ee56f37cd47c5509e85aad9d
Joyce Lau <jshizuki@yahoo.com.hk>
2024-06-08 12:46:29 +0900

    Add solution

---
+++ Ruby/reverse_and_invert.rb
@@ -0,0 +1,9 @@
+def reverse_invert(array)
+  only_integers = array.grep(Integer)
+
+  only_integers.map do |i|
+    i.positive? ? -(i.to_s.chars.reverse.join("").to_i) : (-(i)).to_s.chars.reverse.join("").to_i
+  end
+end
+
+p reverse_invert([1, 12, "a", 3.4, 87, 99.9, -42, 50, 5.6])
