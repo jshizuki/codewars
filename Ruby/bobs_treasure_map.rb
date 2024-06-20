7430c9d79b7e80f479d62ca4e1bd44818b2255bc
Joyce Lau <jshizuki@yahoo.com.hk>
2024-06-20 20:08:33 +0900

    Add solution

--- Ruby/bobs_treasure_map.rb
+++ Ruby/bobs_treasure_map.rb
@@ -1,16 +1,23 @@
 def treasure_finder(map)
   score = { "N" => 1, "E" => 1, "S" => -1, "W" => -1 }
+  opposite_directions = { "N" => "S", "S" => "N", "E" => "W", "W" => "E"}
+  simplified_arr = []
+
   x = 0
   y = 0
 
-  directions = map.to_s.chars
-  filtered_directions = directions.select { |dir| dir == "N" || dir == "E" || dir == "S" || dir == "W" }
+  filtered_directions = map.to_s.chars.select { |dir| dir == "N" || dir == "E" || dir == "S" || dir == "W" }
 
   return nil if filtered_directions.empty?
 
-  filtered_directions.each { |dir| dir == "E" || dir == "W" ? x += score[dir] : y += score[dir] }
+  filtered_directions.each do |dir|
+    !simplified_arr.nil? && simplified_arr.last == opposite_directions[dir] ?
+    simplified_arr.pop : simplified_arr << dir
+  end
+
+  simplified_arr.each { |dir| dir == "E" || dir == "W" ? x += score[dir] : y += score[dir] }
 
   [x, y]
 end
 
-p treasure_finder(1234567)
+p treasure_finder("SENYUSWNEA") #[1,0]
