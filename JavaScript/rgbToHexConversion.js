276ad3db0c93559519e52c817167121cc5e09dcc
Joyce Lau <jshizuki@yahoo.com.hk>
2024-06-30 00:47:09 +0900

    Fix solution

--- JavaScript/rgbToHexConversion.js
+++ JavaScript/rgbToHexConversion.js
@@ -1,16 +1,28 @@
 function rgb(arr) {
-    const letters = Array.from({ length: 6 }, (_, i) => String.fromCharCode(65 + i));
-    const lettersHexConversion = Array.from({ length: 6 }, (_, i) => 10 + i);
-    const hexArray = [];
-    arr.forEach((value, i) => {
-        const hex = Math.floor(value / 16);
-        const firstHex = hex < 10 ? hex : letters[lettersHexConversion.indexOf(hex)];
-        const remainingValue = value - hex * 16;
-        const secondHex = remainingValue < 10
-            ? remainingValue
-            : letters[lettersHexConversion.indexOf(hex)];
-        hexArray.push(firstHex, secondHex);
-    });
-    return hexArray.join("");
+  const letters = Array.from({ length: 6 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  );
+  const lettersHexConversion = Array.from({ length: 6 }, (_, i) => 10 + i);
+
+  const hexArray = [];
+
+  arr.forEach((value, i) => {
+    const hex = Math.floor(value / 16);
+
+    const firstHex =
+      hex < 10 ? hex : letters[lettersHexConversion.indexOf(hex)];
+
+    const remainingValue = value - hex * 16;
+    const secondHex =
+      remainingValue < 10
+        ? remainingValue
+        : letters[lettersHexConversion.indexOf(remainingValue)];
+
+    hexArray.push(firstHex, secondHex);
+  });
+
+  return hexArray.join("");
 }
+
 console.log(rgb([148, 0, 211]));
+console.log(rgb([173, 255, 47]));
