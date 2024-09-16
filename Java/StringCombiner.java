7ab53955d89f96c640e96db03ed88a1ba5ddd2f0
Joyce Lau <jshizuki@yahoo.com.hk>
2024-09-16 19:09:57 +0900

    Added Java and attempted first challenge

---
+++ Java/StringCombiner.java
@@ -0,0 +1,9 @@
+public class StringCombiner {
+  public static String combineNames(String first, String last) {
+    return first + " " + last;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(StringCombiner.combineNames("Joyce", "Lau"));
+  }
+}
