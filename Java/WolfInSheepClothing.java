aab84ef56cb0c9bf3cae76ec86d5d78cab760053
Joyce Lau <jshizuki@yahoo.com.hk>
2024-10-15 11:32:54 +0900

    Add solution

---
+++ Java/WolfInSheepClothing.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class WolfInSheepClothing {
+    public static String warnTheSheep(String[] array) {
+        List<String> list = new ArrayList<>(Arrays.asList(array));
+        Collections.reverse(list);
+        int locationOfWolf = list.indexOf("wolf") + 1;
+
+        return locationOfWolf == 1 ? "Pls go away and stop eating my sheep" :
+                "Oi! Sheep number " + (locationOfWolf - 1) + "! You are about to be eaten by a wolf!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(warnTheSheep(new String[]{"sheep", "sheep", "sheep", "wolf", "sheep"}));
+    }
+}
+
